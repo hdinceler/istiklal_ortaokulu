17d1200096c9d9eaf2a17d083b788fee45f734d9
hdinceler <55253026+hdinceler@users.noreply.github.com>
2024-02-15 14:24:51 +0300

    Add files via upload

--- NRF+LCD+KUMANDA+ARABA/Araba/Araba.ino
+++ NRF+LCD+KUMANDA+ARABA/Araba/Araba.ino
@@ -4,16 +4,14 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
- 
-#define yakinlik_siniri  10   // araba bu cm cinsinden degerden  daha yakın cisim algılarsa dursun
-long zaman;
-long mesafe;
+
+#define yakinlik_siniri  5   // araba bu cm cinsinden degerden  daha yakın cisim algılarsa dursun
+long zaman=0;
+long mesafe_on=0;
+long mesafe_arka=0;
 unsigned long baslangic_zamani_millis=0;;
 unsigned long baslangic_zamani_micros=0;;
-bool trigDurumu = false;
 char buff[10];
-
- 
 
 uint8_t x_degeri=0;
 uint8_t y_degeri=0;
@@ -23,32 +21,38 @@ uint8_t asagi_buton=0;
 uint8_t sag_buton=0;
 uint8_t sol_buton=0;
 
-int pin_mesafe_trig = 2;
-int pin_mesafe_echo = 4;
-#define pin_far 2
+#define pin_on_far 2
 #define pin_sag_teker_ileri 3 //~PWM
 #define pin_korna 4
 #define pin_sag_teker_geri 5 //~PWM
 #define pin_sol_teker_ileri 6 //~PWM
-#define CE_PIN 8 // NRF için
 #define CSN_PIN 7 // NRF için
+#define CE_PIN 8 // NRF için
 #define pin_sol_teker_geri 9 //~PWM
+#define pin_sis_fari 10
+
+uint8_t pin_mesafe_echo_on = A0;
+uint8_t pin_mesafe_trig_on = A1;
+uint8_t pin_mesafe_echo_arka = A2;
+uint8_t pin_mesafe_trig_arka = A3;
+#include "araba_kutuphane.h"
 
 RF24 radio(CE_PIN , CSN_PIN); // CE, CSN pimleri
 char gelen_komut[32] = "";
 
 void setup() {
-  
-  pinMode(pin_mesafe_trig, OUTPUT);
-  pinMode(pin_mesafe_echo, INPUT);
-
-  pinMode( pin_far, OUTPUT);
-  pinMode( pin_korna , OUTPUT);
+  pinMode( pin_on_far, OUTPUT);
   pinMode( pin_sag_teker_ileri , OUTPUT);
+  pinMode( pin_korna , OUTPUT);
   pinMode( pin_sag_teker_geri , OUTPUT);
   pinMode( pin_sol_teker_ileri , OUTPUT);
   pinMode( pin_sol_teker_geri , OUTPUT);
+  pinMode( pin_sis_fari, OUTPUT);
 
+  pinMode(pin_mesafe_trig_on, OUTPUT);
+  pinMode(pin_mesafe_echo_on, INPUT);
+  pinMode(pin_mesafe_trig_arka, OUTPUT);
+  pinMode(pin_mesafe_echo_arka, INPUT);
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(1, 0xF0F0F0F0E12E); // Vericinin yazma hattı adresi
@@ -65,8 +69,8 @@ void loop() {
 //RADYO İLETİŞİM
   if (radio.available()) {
     
-    radio.read(&gelen_komut, sizeof(gelen_komut));
-    
+    radio.read(&gelen_komut, sizeof(gelen_komut));   
+    //gelen komut cümlesi parçalara ayrıştırılıyor
     strcpy( buff , strtok( gelen_komut,  "," ) ); x_degeri= atoi(buff);
     strcpy( buff , strtok(NULL,  "," ) ); y_degeri= atoi(buff);
     strcpy( buff , strtok(NULL,  "," ) ); joy_buton= atoi(buff);
@@ -74,10 +78,22 @@ void loop() {
     strcpy( buff , strtok(NULL,  "," ) ); asagi_buton= atoi(buff);
     strcpy( buff , strtok(NULL,  "," ) ); sag_buton = atoi(buff);
     strcpy( buff , strtok(NULL,  "," ) ); sol_buton= atoi(buff);
+    //  komut parçaları çıkış pinlerine dağıtılıyor
+    digitalWrite(pin_on_far , yukari_buton );
+    digitalWrite(pin_sis_fari,sol_buton );
+    digitalWrite(pin_korna, sag_buton);
 
+  //motor sürücü yönetimi
+  if (y_degeri >=526 && y_degeri < 1024){   ileri_git( map(y_degeri, 526,1023, 0,255) ); } 
+  else if (y_degeri >= 0 && y_degeri < 520) {geri_git(map(y_degeri, 520, 0, 0, 255)); }
+  else if (x_degeri >= 0 && x_degeri < 504) {sola_don(map(x_degeri, 504, 0, 0, 255)); }
+  else if (x_degeri > 510 && x_degeri < 1024) {saga_don(map(x_degeri, 510, 1023, 0, 255)); }
+  else dur();
+  
+    //LCD ekrana konum yazdırma işlemleri
     Serial.println(gelen_komut);
-     lcd.setCursor(0,0);lcd.print("X"); //Joystick X exseni
-    lcd.setCursor(4,0);lcd.print("Y");  //Joystick Y ekseni
+    lcd.setCursor(1,0);lcd.print("X"); //Joystick X exseni
+    lcd.setCursor(5,0);lcd.print("Y");  //Joystick Y ekseni
     lcd.setCursor(8,0);lcd.print("J");  //Joystick butonu
     lcd.setCursor(10,0);lcd.print("U");  //yukarı düğme
     lcd.setCursor(11,0);lcd.print("D");  //aşağı düğme
